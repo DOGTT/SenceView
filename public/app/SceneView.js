c59c3873145b1828d3a406844e223c01fd17b7fe
DOGTT <bolitt@hotmail.com>
2017-08-03 23:13:53 +0800

    m

--- public/app/SceneView.js
+++ public/app/SceneView.js
@@ -1,5 +1,16 @@
 var SceneView = function() {
     //init
     //rander
+    var renderer;
+
+    function init() {
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.sortObjects = false;
+        c1_canvas_div = document.getElementById('viewBox');
+        c1_canvas_div.appendChild(renderer.domElement);
+    }
+
     //show
 }();
