11e39b608030642636a6bf725790701fe91a0b2a
DOGTT <bolitt@hotmail.com>
2017-08-04 14:25:05 +0800

    m

--- public/app/SceneView.js
+++ public/app/SceneView.js
@@ -3,6 +3,10 @@ var SceneView = function() {
     //rander
     var renderer;
     var c1_canvas_div;
+    var scene, camera;
+    var viewSize = new THREE.Vector2(window.innerWidth, window.innerHeight);
+    var windowHalfX = viewSize.x / 2;
+    var windowHalfY = viewSize.y / 2;
 
     function init() {
         renderer = new THREE.WebGLRenderer();
@@ -11,7 +15,40 @@ var SceneView = function() {
         renderer.sortObjects = false;
         c1_canvas_div = document.getElementById('viewBox');
         c1_canvas_div.appendChild(renderer.domElement);
+
+        //scene
+        scene = new THREE.Scene();
+        scene.fog = new THREE.Fog(0xff0000, 0, 2000);
+        universeScene = new THREE.Scene();
+        //camera
+        camera = new THREE.PerspectiveCamera(45, viewSize.x / viewSize.y, 1, 2000);
+        camera.position.set(0, 0, 100);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+        scene.add(camera);
+
+        //light
+        var ambient = new THREE.AmbientLight(0xcccccc);
+        scene.add(ambient);
+
+        var geo = new THREE.BoxGeometry(200, 200, 200);
+        var mat = new THREE.MeshPhongMaterial({
+            color: 0xffff00,
+            wireframe: true
+        });
+        var ma = new THREE.Mesh(geo, mat);
+        scene.add(ma);
     }
     init();
+    animate();
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+        //stats.update();
+    }
+
+    function render() {
+        renderer.render(scene, camera);
+    }
     //show
 }();
